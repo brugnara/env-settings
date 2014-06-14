31dcac36c9c682164a73ab2a600683aa15428f1f
djhell <daniele@brugnara.me>
2014-06-14 12:32:19 +0200

    fix recursively merge

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var path = require('path');
 var mainPath = path.dirname(process.mainModule.filename);
 var cfg = require(mainPath + '/config/settings.json');
-var merge = require('merge');
+var merge = require('merge-recursive');
 
-var settings = merge(settings, cfg.default);
+var settings = cfg.default;
 var ENV = process.env.NODE_ENV || 'development';
 
 try {
-  settings = merge(settings, cfg[ENV]);
+  settings = merge.recursive(settings, cfg[ENV]);
 } catch(e) {
   // No env detected. Issuing a warning.
   console.error('Environment specific settings not found: %s', ENV);
